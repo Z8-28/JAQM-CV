c34b0c1e30c18b586196ac6f45911a6492c8d400
Z8-28 <114176071+Z8-28@users.noreply.github.com>
2025-02-03 17:12:22 -0600

    prueba de creacion demo
    prueba de creacion demo

---
+++ demo/JS/redirect.js
@@ -0,0 +1,15 @@
+function geo() {
+    location.href = 'index.html';
+}
+
+function read() {
+    location.href = 'leer_boleta_view.html';
+}
+
+function show() {
+    location.href = 'mostrar_boleta_view.html';
+}
+
+function test() {
+    location.href = 'test.html';
+}
