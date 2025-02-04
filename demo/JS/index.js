56b46972145ad90b4037acc9ec99a173f15d83b5
Z8-28 <114176071+Z8-28@users.noreply.github.com>
2025-02-04 13:31:00 -0600

    demo
    demo

--- demo/JS/index.js
+++ demo/JS/index.js
@@ -1,12 +1,18 @@
 function menu_size() {
     let menu = document.getElementById("menu");
     let screen = document.getElementById("sub_screen");
-    let btn = document.getElementsByClassName("button_menu")
-    if (menu.style.width == "0px"){
+    let btn = document.getElementsByClassName("button_menu");
+    if (menu.style.width == "0px") {
+        menu.style.minWidth = "200px";
         menu.style.width = "15%";
         btn.style.width = "95%";
         screen.style.width = "85%";
-    }else {
+        if (screen.style.width < "50vw") {
+            menu.style.width = "100vw";
+            screen.style.width = "0vw";
+        }
+    } else {
+        menu.style.minWidth = "0px";
         menu.style.width = "0px";
         btn.style.width = "0px";
         screen.style.width = "100vw";
